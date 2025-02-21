fd8d2a434bc47f8e219516f6ab73bddf9c203506
TactorDev <119103845+tactordev@users.noreply.github.com>
2025-02-21 12:18:02 +0000

    Bot join/leave log

--- events/joins.py
+++ events/joins.py
@@ -9,5 +9,9 @@ class joins(commands.Cog):
     async def on_guild_join(self, guild):
         print(f"Bot has joined {guild.name}.")
 
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        print(f"Bot has left {guild.name}.")
+
 async def setup(client):
     await client.add_cog(joins(client))
