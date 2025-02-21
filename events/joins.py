6db4af0b94f1b19d7c9e50aa482148abfdc33765
TactorDev <119103845+tactordev@users.noreply.github.com>
2025-02-21 12:21:34 +0000

    Fixed join-leave logs

--- events/joins.py
+++ events/joins.py
@@ -1,17 +1,23 @@
 import discord
 from discord.ext import commands
+from utils.loadConfig import loadConfig
 
 class joins(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.config = loadConfig()
 
     @commands.Cog.listener()
-    async def on_guild_join(self, guild):
-        print(f"Bot has joined {guild.name}.")
+    async def on_guild_join(self, joinGuild):
+        guild = self.client.get_guild(self.config["primaryGuild"])
+        channel = guild.get_channel(self.config["join-leave-logs"])
+        await channel.send(embed = discord.Embed(title = "Bot Joined", description=f"Bot has joined {joinGuild.name}.", color = discord.Color.green()))
 
     @commands.Cog.listener()
-    async def on_guild_remove(self, guild):
-        print(f"Bot has left {guild.name}.")
+    async def on_guild_remove(self, leaveGuild):
+        guild = self.client.get_guild(self.config["primaryGuild"])
+        channel = guild.get_channel(self.config["join-leave-logs"])
+        await channel.send(embed = discord.Embed(title = "Bot Left", description=f"Bot has left {leaveGuild.name}.", color = discord.Color.red()))
 
 async def setup(client):
     await client.add_cog(joins(client))
